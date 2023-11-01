3f1b19c4589a950b3de3ba66f953fb20013c99d1
mazhar <mazharalhayek12@gmail.com>
2023-11-01 20:53:00 +0200

    ministry_f_higher_education institution table

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InstitutionsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+                //---------Institutions routes----------//
+//make sure your Api link is something like this: http://127.0.0.1:8000/api/institutions/inst
+Route::group(['prefix'=>'institutions'],function(){
+//show all institutions
+Route::get('/inst',[InstitutionsController::class,'index']);
+//show a specific institution
+Route::get('/inst/{instid}',[InstitutionsController::class,'show']);
+//insert new institution
+Route::post('/inst',[InstitutionsController::class,'store']);
+//update existing institution
+Route::post('/inst/{instid}',[InstitutionsController::class,'update']);
+//remove existing institution
+Route::delete('/inst/{instid}',[InstitutionsController::class,'destroy']);
+//remove all Institutions
+Route::get('/destroyall',[InstitutionsController::class,'destroy_all']);
 });
